28973cb0d8863fc999abe7da6bf52627bd3bc002
Rohan Malo <64rohanmalo@gmail.com>
2026-02-05 13:21:47 +0530

    build bug fix

---
+++ tilesecommerse/src/app/(user)/Gallery/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GalleryPage = () => {
+  return (
+    <div>
+      <h1>Gallery Page</h1>
+      <p>This is the gallery page.</p>
+    </div>
+  );
+};
+
+export default GalleryPage;
