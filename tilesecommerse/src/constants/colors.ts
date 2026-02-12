5e98672c68204e5cae6e263cbb820c24ca4556a6
Rohan Malo <64rohanmalo@gmail.com>
2026-02-12 08:50:07 +0530

    more colors added for dropdown

--- tilesecommerse/src/constants/colors.ts
+++ tilesecommerse/src/constants/colors.ts
@@ -3,24 +3,88 @@ interface ColorMapping {
 }
 
 export const colorMapping: ColorMapping = {
-  black: "#1A1A1A",
-  white: "#FAFAFA",
-  "off white": "#F8F8F8",
-  ivory: "#FFFFF0",
-  cement: "#8A8A8A",
-  silver: "#C0C0C0",
-  taupe: "#483C32",
-  terracotta: "#E2725B",
-  charcoal: "#36454F",
-  walnut: "#5C4033",
-  choco: "#D2691E",
-  blue: "#6B8CAE",
-  "navy blue": "#2C3E50",
-  green: "#5D7B6F",
-  "light green": "#A8C5B5",
-  grey: "#9CA3AF",
-  ecru: "#F0EBE3",
-  anthracite: "#4A4A4A",
-  beige: "#E8E0D5",
-  "dark anthracite": "#363636",
+  // Whites & Off-Whites
+  "White": "#FFFFFF",
+  "Off White": "#F8F8F8",
+  "Ivory": "#FFFFF0",
+  "Cream": "#FFFDD0",
+  "Almond": "#EFDECD",
+  "Pearl": "#F0EAD6",
+  "Linen": "#FAF0E6",
+  "Chalk": "#F5F5DC",
+  "Ecru": "#F0EBE3",
+
+  // Greys
+  "Light Grey": "#D3D3D3",
+  "Grey": "#9CA3AF",
+  "Dark Grey": "#6B7280",
+  "Charcoal": "#36454F",
+  "Graphite": "#383838",
+  "Smoke": "#738276",
+  "Slate": "#708090",
+  "Ash Grey": "#B2BEB5",
+  "Anthracite": "#4A4A4A",
+  "Dark Anthracite": "#363636",
+
+  // Black
+  "Black": "#1A1A1A",
+
+  // Beiges & Neutrals
+  "Beige": "#E8E0D5",
+  "Sand": "#C2B280",
+  "Taupe": "#483C32",
+  "Greige": "#B8B5A8",
+  "Cappuccino": "#A67B5B",
+  "Mushroom": "#C9B5A0",
+  "Putty": "#C8B89C",
+
+  // Browns
+  "Brown": "#8B4513",
+  "Light Brown": "#B5651D",
+  "Dark Brown": "#654321",
+  "Walnut": "#5C4033",
+  "Teak": "#B5651D",
+  "Oak": "#C19A6B",
+  "Wenge": "#3E2723",
+  "Chocolate": "#7B3F00",
+  "Choco": "#D2691E",
+  "Mocha": "#967969",
+  "Coffee": "#6F4E37",
+
+  // Stone & Concrete
+  "Marble": "#F5F5F5",
+  "Stone": "#928E85",
+  "Cement": "#8A8A8A",
+  "Concrete": "#A9A9A9",
+  "Travertine": "#E8D5C4",
+  "Pebble": "#B8B8B0",
+
+  // Metallics
+  "Chrome": "#E5E4E2",
+  "Stainless Steel": "#C0C0C0",
+  "Silver": "#C0C0C0",
+  "Gold": "#FFD700",
+  "Rose Gold": "#B76E79",
+  "Gunmetal": "#2C3539",
+  "Copper": "#B87333",
+  "Bronze": "#CD7F32",
+  "Brushed Nickel": "#A8A9AD",
+  "Antique Bronze": "#665D1E",
+
+  // Blues
+  "Blue": "#6B8CAE",
+  "Navy Blue": "#2C3E50",
+  "Sky Blue": "#87CEEB",
+  "Teal": "#008080",
+
+  // Greens
+  "Green": "#5D7B6F",
+  "Olive Green": "#556B2F",
+  "Bottle Green": "#006A4E",
+  "Mint": "#98FF98",
+  "Light Green": "#A8C5B5",
+
+  // Reds
+  "Red": "#DC143C",
+  "Terracotta": "#E2725B",
 };
